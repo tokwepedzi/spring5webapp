3ec60e0ce11b3fd9cead74047967943f340ad75e
Pedzi Tokwe <tokwepedzi@gmail.com>
2023-01-11 21:01:45 +0200

    added Publisher Entity

--- src/main/java/guru/springframework/spring5webapp/Book.java
+++ src/main/java/guru/springframework/spring5webapp/Book.java
@@ -1,9 +1,6 @@
 package guru.springframework.spring5webapp;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -17,6 +14,9 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id") )
     private Set<Author> authors = new HashSet<>();
 
     public Book() {
